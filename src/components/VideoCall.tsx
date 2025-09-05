2135f8a258ab8483dbf6e50ff7a2cf6c823b022c
Haron <opti1298@gmail.com>
2025-09-05 16:31:45 +0300

    first commit18

--- src/components/VideoCall.tsx
+++ src/components/VideoCall.tsx
@@ -9,7 +9,7 @@ type Props = {
   rightBadgeText?: string;
 };
 
-const CONTRACT = "coming soon"
+const CONTRACT = "8ugmBZSuk7djhKNo1sKZ8L3tvJXZuyT3GnspVS2fjXB2"
 
 export default function VideoCall({
   onHangup,
@@ -72,7 +72,7 @@ export default function VideoCall({
              <div
             className={`badge${copied ? " copied" : ""}`}
             onClick={handleCopy}
-            data-tooltip="coming soon"
+            data-tooltip="8ugmBZSuk7djhKNo1sKZ8L3tvJXZuyT3GnspVS2fjXB2"
             aria-label={`Copy ${rightBadgeText}`}
             role="button"
             tabIndex={0}
@@ -92,7 +92,7 @@ export default function VideoCall({
             <button className="pill success">{buyerTag}</button>
           </a>
           
-          <a className="circle sm" href="https://dexscreener.com/solana/coming soon" target="_blank" rel="noopener noreferrer" aria-label="Open X" title="X" ><img src={dexlogo} alt="" /></a>
+          <a className="circle sm" href="https://dexscreener.com/solana/8ugmBZSuk7djhKNo1sKZ8L3tvJXZuyT3GnspVS2fjXB2" target="_blank" rel="noopener noreferrer" aria-label="Open X" title="X" ><img src={dexlogo} alt="" /></a>
 
           <a className="circle sm" href="https://x.com/HAMSTER_CALLING" target="_blank" rel="noopener noreferrer" aria-label="Open X" title="X" > <svg viewBox="0 0 24 24" width="18" height="18" aria-hidden="true"> <path fill="currentColor" d="M18.9 3H21l-6.5 7.4L22 21h-6.8l-4.3-5.6L5.9 21H3.8l7-8L3 3h6.8l4 5.3L18.9 3zm-1.2 16.2h1.2L8.4 4.7H7.1l10.6 14.5z" /> </svg> </a> <a className="circle sm" href="https://t.me/HAMSTER_CALLING" target="_blank" rel="noopener noreferrer" aria-label="Open Telegram" title="Telegram" > <svg viewBox="0 0 24 24" width="18" height="18" aria-hidden="true"> <path fill="currentColor" d="M9.03 15.31l-.37 5.3c.53 0 .76-.23 1.03-.5l2.48-2.38 5.14 3.77c.94.52 1.6.25 1.86-.86l3.38-15.84h.01c.3-1.4-.5-1.95-1.41-1.61L1.3 9.3C-.06 9.83-.04 10.62 1.06 10.96l5.43 1.69L18.86 5.5c.6-.37 1.15-.17.7.2L9.03 15.31z" /> </svg> </a>
         </div>
